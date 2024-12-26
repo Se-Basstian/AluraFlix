5dc9968c33f472cfc201ab3f1dad55aadf02d4d3
Se-Basstian <p.sebastian.23.11.99@gmail.com>
2024-12-26 10:20:04 -0500

    Inicia el proyecto con «react-router»

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Routes } from "react-router";
+
+function App() {
+  return (
+    <Routes>
+      <h1>Inicio del proyecto</h1>
+    </Routes>
+  );
+}
+
+export default App;
