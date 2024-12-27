9899c64184d3f524c837643ab4ce2689fc49981b
Se-Basstian <p.sebastian.23.11.99@gmail.com>
2024-12-26 23:35:02 -0500

    Componente <Borron/> adaptable

--- src/components/common/Button/index.tsx
+++ src/components/common/Button/index.tsx
@@ -1,26 +1,38 @@
 import { Link } from "react-router";
 import styles from "./styles.module.css";
+import type { ReactNode } from "react";
+// import { RiHomeLine } from "react-icons/ri";
 
 interface Props {
   tipo?: "normal" | "enlace";
   to?: string;
   valor: string;
   selecionado?: boolean;
+  startIcon?: ReactNode;
+  onClick: () => void;
 }
 
-function Button({ tipo = "normal", valor, to = "", selecionado }: Props) {
+function Button({
+  tipo = "normal",
+  valor,
+  to = "",
+  selecionado,
+  startIcon,
+  onClick,
+}: Props) {
   const clasesButton = () => {
     const clase: string[] = [];
 
     clase.push(styles.button);
 
-    if (selecionado) clase.push(styles.seleccionado);
+    if (selecionado) clase.push(styles.activo);
 
     return clase.join(" ");
   };
 
   return (
-    <button className={clasesButton()} type="button">
+    <button className={clasesButton()} type="button" onClick={onClick}>
+      {startIcon}
       {tipo !== "enlace" ? (
         valor
       ) : (
