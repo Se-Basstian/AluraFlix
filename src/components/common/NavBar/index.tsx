122715531a610460b5d61e14aed686eda2e51f9b
Se-Basstian <p.sebastian.23.11.99@gmail.com>
2024-12-26 23:35:32 -0500

    Componente <NavBar/> adaptable

--- src/components/common/NavBar/index.tsx
+++ src/components/common/NavBar/index.tsx
@@ -1,15 +1,62 @@
 import logo from "../../../assets/imgs/logo_AluraFlix.png";
 import style from "./style_NavBar.module.css";
 import Button from "../Button";
+import { GrHomeRounded } from "react-icons/gr";
+import { FiPlusCircle } from "react-icons/fi";
+import { useState } from "react";
 
 function NavBar() {
+  const [cambio, setCambio] = useState(true);
+  const [anchoVentana, setAnchoVentana] = useState(window.innerWidth);
+
+  window.addEventListener("resize", () => {
+    setAnchoVentana(window.innerWidth);
+  });
+
   return (
     <nav className={style.nav}>
       <img className={style.img} src={logo} alt="Logo AluraFlix" />
 
       <div className={style.divGrupoBotones}>
-        <Button tipo="enlace" to="/" valor="Home" selecionado />
-        <Button tipo="enlace" to="/nuevo" valor="Nuevo Video" />
+        {cambio && anchoVentana <= 470 ? (
+          <GrHomeRounded
+            className={[style.homeSolo, style.activo].join(" ")}
+            onClick={() => {
+              setCambio(!cambio);
+            }}
+          />
+        ) : (
+          <Button
+            tipo="enlace"
+            to="/"
+            valor="Home"
+            startIcon={<GrHomeRounded className={style.home} />}
+            selecionado={!cambio}
+            onClick={() => {
+              setCambio(!cambio);
+            }}
+          />
+        )}
+
+        {!cambio && anchoVentana <= 470 ? (
+          <FiPlusCircle
+            className={[style.plusSolo, cambio && style.activo].join(" ")}
+            onClick={() => {
+              setCambio(!cambio);
+            }}
+          />
+        ) : (
+          <Button
+            tipo="enlace"
+            to="/"
+            valor="Nuevo Video"
+            startIcon={<FiPlusCircle className={style.plus} />}
+            selecionado={cambio}
+            onClick={() => {
+              setCambio(!cambio);
+            }}
+          />
+        )}
       </div>
     </nav>
   );
