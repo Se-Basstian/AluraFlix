6fde69c6b3c4af690fe677632c8d3b2c2012e624
Se-Basstian <p.sebastian.23.11.99@gmail.com>
2024-12-27 20:46:37 -0500

    Botón limpio

--- src/components/common/Button/index.tsx
+++ src/components/common/Button/index.tsx
@@ -1,45 +1,27 @@
-import { Link } from "react-router";
 import styles from "./styles.module.css";
 import type { ReactNode } from "react";
-// import { RiHomeLine } from "react-icons/ri";
 
 interface Props {
-  tipo?: "normal" | "enlace";
-  to?: string;
   valor: string;
-  selecionado?: boolean;
   startIcon?: ReactNode;
-  onClick: () => void;
+  activo?: boolean;
 }
 
-function Button({
-  tipo = "normal",
-  valor,
-  to = "",
-  selecionado,
-  startIcon,
-  onClick,
-}: Props) {
+function Button({ activo, valor, startIcon }: Props) {
   const clasesButton = () => {
     const clase: string[] = [];
 
     clase.push(styles.button);
 
-    if (selecionado) clase.push(styles.activo);
+    if (activo) clase.push(styles.activo);
 
     return clase.join(" ");
   };
 
   return (
-    <button className={clasesButton()} type="button" onClick={onClick}>
+    <button className={clasesButton()} type="button">
       {startIcon}
-      {tipo !== "enlace" ? (
-        valor
-      ) : (
-        <Link className={styles.link} to={to}>
-          {valor}
-        </Link>
-      )}
+      {valor}
     </button>
   );
 }
