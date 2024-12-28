572cdd2ea4c186a94612f3e28154469e464118df
Se-Basstian <p.sebastian.23.11.99@gmail.com>
2024-12-27 20:47:15 -0500

    Limpindo código

--- src/components/common/NavBar/index.tsx
+++ src/components/common/NavBar/index.tsx
@@ -3,60 +3,27 @@ import style from "./style_NavBar.module.css";
 import Button from "../Button";
 import { GrHomeRounded } from "react-icons/gr";
 import { FiPlusCircle } from "react-icons/fi";
-import { useState } from "react";
+import { Link } from "react-router";
 
 function NavBar() {
-  const [cambio, setCambio] = useState(true);
-  const [anchoVentana, setAnchoVentana] = useState(window.innerWidth);
-
-  window.addEventListener("resize", () => {
-    setAnchoVentana(window.innerWidth);
-  });
-
   return (
     <nav className={style.nav}>
       <img className={style.img} src={logo} alt="Logo AluraFlix" />
 
       <div className={style.divGrupoBotones}>
-        {cambio && anchoVentana <= 470 ? (
-          <GrHomeRounded
-            className={[style.homeSolo, style.activo].join(" ")}
-            onClick={() => {
-              setCambio(!cambio);
-            }}
-          />
-        ) : (
+        <Link to="/">
           <Button
-            tipo="enlace"
-            to="/"
             valor="Home"
             startIcon={<GrHomeRounded className={style.home} />}
-            selecionado={!cambio}
-            onClick={() => {
-              setCambio(!cambio);
-            }}
           />
-        )}
+        </Link>
 
-        {!cambio && anchoVentana <= 470 ? (
-          <FiPlusCircle
-            className={[style.plusSolo, cambio && style.activo].join(" ")}
-            onClick={() => {
-              setCambio(!cambio);
-            }}
-          />
-        ) : (
+        <Link to="/nuevo_video">
           <Button
-            tipo="enlace"
-            to="/"
             valor="Nuevo Video"
             startIcon={<FiPlusCircle className={style.plus} />}
-            selecionado={cambio}
-            onClick={() => {
-              setCambio(!cambio);
-            }}
           />
-        )}
+        </Link>
       </div>
     </nav>
   );
